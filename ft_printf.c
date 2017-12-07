592cc18acc87fadc7a1bfb6b69e869f705d3bedf
Slavik ZVERHOVSKY <szverhov@e1r3p4.unit.ua>
2017-12-07 21:45:51 +0200

    a

--- ft_printf.c
+++ ft_printf.c
@@ -305,13 +305,13 @@ void type_S(va_list *list, t_flags **mody)
 		ft_putstr("(null)");
 		return ;
 	}
-	//check_err_S(tmp);
+	check_err_S(tmp);
 	if (g_err == 1)
 	{
 		free(tmp);
 		return ;
 	}
-	ft_putgstr();
+	//ft_putgstr();
 	if ((*mody)->flags[4] == 1 && (*mody)->flags[0] != 1)
 		filler = '0';
 	if ((*mody)->precision_len == 0 && (*mody)->spec_flag == 0)
@@ -1142,16 +1142,16 @@ void type_X(va_list *list, t_flags **mody)
 	free(res);
 }
 
-// int main(int argc, char const *argv[])
-// {
-// 	setlocale(LC_ALL, "en_US.UTF-8");
-// 	 ft_printf("%C", L'�');
-// 	 ft_printf("%S", L"@@");
+int main(int argc, char const *argv[])
+{
+	setlocale(LC_ALL, "en_US.UTF-8");
+	 ft_printf("%C", L'�');
+	// ft_printf("%S", L"@@");
 
 	
-// 	//ft_printf("%S", L"aa");
-// 	return 0;
-// }
+	//ft_printf("%S", L"aa");
+	return 0;
+}
 
 void type_c(va_list *list, t_flags **mody)
 {
@@ -1185,6 +1185,8 @@ void type_c(va_list *list, t_flags **mody)
 		ft_putchar(c);
 		return ;
 	}
+	printf("%d\n",c );
+	return;
 	ft_putchar(c);
 }
 
