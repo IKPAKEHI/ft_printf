710451a8c0c4a3e163b1058d88ab1100fb678528
Slavik ZVERHOVSKY <szverhov@e1r3p4.unit.ua>
2017-12-11 13:37:47 +0200

    asd

--- type_handlers/type_C.c
+++ type_handlers/type_C.c
@@ -15,27 +15,58 @@
 void type_C(va_list *list, t_flags **mody)
 {
 
-	type_c(list, &(*mody));
-
+	wchar_t c;
+	int size;
+	char res;
+	 
+	
+	//size = ft_wchar_size(c);
+	if ((*mody)->precision_len > 0)
+	{
+		type_c(list, &(*mody));
+		return ;
+		//size -= (*mody)->precision_len;
+	}
+	if ((*mody)->precision_len >= size)
+	{
+		//size = 1;
+	}
+		//(*mody)->width_len -= ft_wchar_size(c);
+	c = va_arg(*list, wchar_t);
+	if ((*mody)->width_len > 0 && (*mody)->flags[0] == 1)
+	{
+		ft_putwchar(c);
+		while ((*mody)->width_len--)
+			ft_putwchar(' ');
+		return ;
+	}
+	if ((*mody)->width_len > 0 & (*mody)->flags[0] != 1)
+	{
+		while ((*mody)->width_len--)
+			ft_putwchar(' ');
+		ft_putwchar(c);
+		return ;
+	}
+	ft_putwchar(c);
 	// wchar_t c;
 
 	// *mody = *mody;
 	// c = va_arg(*list, wchar_t);
-	// // if ((*mody)->width_star)
-	// // 	(*mody)->width_len = va_arg(*list, int);
-	// // if ((*mody)->width_len > 0 && (*mody)->flags[0] == 1)
-	// // {
-	// // 	ft_putwchar(c);
-	// // 	while (--(*mody)->width_len)
-	// // 		ft_putwchar(' ');
-	// // 	return ;
-	// // }
-	// // if ((*mody)->width_len > 0 & (*mody)->flags[0] != 1)
-	// // {
-	// // 	while (--(*mody)->width_len)
-	// // 		ft_putwchar(' ');
-	// // 	ft_putwchar(c);
-	// // 	return ;
-	// // }
+	// if ((*mody)->width_star)
+	// 	(*mody)->width_len = va_arg(*list, int);
+	// if ((*mody)->width_len > 0 && (*mody)->flags[0] == 1)
+	// {
+	// 	ft_putwchar(c);
+	// 	while (--(*mody)->width_len)
+	// 		ft_putwchar(' ');
+	// 	return ;
+	// }
+	// if ((*mody)->width_len > 0 & (*mody)->flags[0] != 1)
+	// {
+	// 	while (--(*mody)->width_len)
+	// 		ft_putwchar(' ');
+	// 	ft_putwchar(c);
+	// 	return ;
+	// }
 	// ft_putwchar(c);
 }
