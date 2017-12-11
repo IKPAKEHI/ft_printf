8e59710ec8a1c43830a2d92ca8f6a6940f19bfff
Slavik ZVERHOVSKY <szverhov@e1r3p4.unit.ua>
2017-12-11 13:39:24 +0200

    asd

--- type_handlers/type_C.c
+++ type_handlers/type_C.c
@@ -21,7 +21,7 @@ void type_C(va_list *list, t_flags **mody)
 	 
 	
 	//size = ft_wchar_size(c);
-	if ((*mody)->precision_len > 0)
+	if ((*mody)->precision_len > 0 || (*mody)->spec_flag == 0)
 	{
 		type_c(list, &(*mody));
 		return ;
