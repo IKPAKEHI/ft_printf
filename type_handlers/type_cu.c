11bb67880d9545134f02f540f3e8ae1b4b902034
Slavik ZVERHOVSKY <szverhov@e2r8p3.unit.ua>
2017-12-12 14:51:31 +0200

    asd

--- type_handlers/type_cu.c
+++ type_handlers/type_cu.c
@@ -12,7 +12,43 @@
 
 #include "../ft_printf.h"
 
+int		ft_wchar_size(wchar_t c)
+{
+	int res;
+
+	res = 0;
+	if (c < 128)
+		res += 1;
+	if (c > 127 && c < 2048)
+		res += 2;
+	if (c > 2047 && c < 65536)
+		res += 3;
+	if (c > 65535 && c < 2097152)
+		res += 4;
+	return (res);
+}
+
 void	type_cu(va_list *list, t_flags **mody)
 {
-	type_c(list, &(*mody));
+	wchar_t c;
+
+	if (mody == 0)
+		return ;
+	c = va_arg(*list, wchar_t);
+	(*mody)->width_len -= ft_wchar_size(c);
+	if ((*mody)->width_len > 0 && (*mody)->flags[0] == 1)
+	{
+		ft_putwchar(c);
+		while ((*mody)->width_len--)
+			ft_putwchar(' ');
+		return ;
+	}
+	if ((*mody)->width_len > 0 & (*mody)->flags[0] != 1)
+	{
+		while ((*mody)->width_len--)
+			ft_putwchar(' ');
+		ft_putwchar(c);
+		return ;
+	}
+	ft_putwchar(c);
 }
